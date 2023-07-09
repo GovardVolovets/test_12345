7ff5277154392289cd8a834dc056d6bf76e32c46
GovardVolovets <govard.volovets@gmail.com>
2023-07-09 12:48:54 +0300

    Initial commit

---
+++ src/main/java/ru/perfomance/lab/Controller/MainController.java
@@ -0,0 +1,36 @@
+package ru.perfomance.lab.Controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.perfomance.lab.Model.RequestDTO;
+
+import java.math.BigDecimal;
+
+@RestController
+public class MainController {
+
+    private Logger log = LoggerFactory.getLogger(MainController.class);
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @PostMapping (
+            value = "/info/postBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object postBalances(@RequestBody RequestDTO requestDTO) {
+        try {
+            String clientId = requestDTO.getClientId();
+            char firstDigit = clientId.charAt(0);
+            BigDecimal maxLimit;
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/perfomance/lab/Model/ResponseDTO.java
@@ -0,0 +1,19 @@
+package ru.perfomance.lab.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ResponseDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String currency;
+    private String balance;
+    private String maxLimit;
+}
