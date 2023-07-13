a0eaf407cfab307bfc35d5e61999875ac49829df
GovardVolovets <govard.volovets@gmail.com>
2023-07-13 21:09:25 +0300

    change MainController

--- src/main/java/ru/perfomance/lab/Controller/MainController.java
+++ src/main/java/ru/perfomance/lab/Controller/MainController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import ru.perfomance.lab.Model.RequestDTO;
 import ru.perfomance.lab.Model.ResponseDTO;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class MainController {
@@ -21,6 +23,8 @@ public class MainController {
     private Logger log = LoggerFactory.getLogger(MainController.class);
 
     ObjectMapper mapper = new ObjectMapper();
+
+    public long start_time = 0L;
 
     @PostMapping (
             value = "/info/postBalances",
@@ -55,11 +59,58 @@ public class MainController {
             log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
             log.error("***** Ответ *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
 
+            long pacing = ThreadLocalRandom.current().nextLong(100, 500);
+            long end_time = System.currentTimeMillis();
+            if (end_time - start_time < pacing)
+                Thread.sleep(pacing - (end_time - start_time));
+
             return responseDTO;
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
 
+    @GetMapping(
+            value = "/info/getBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object getBalances(@RequestBody RequestDTO requestDTO) {
+        try {
+            String clientId = requestDTO.getClientId();
+            char firstDigit = clientId.charAt(0);
+            BigDecimal maxLimit;
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000.00);
+            } else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000.00);
+            } else {
+                maxLimit = new BigDecimal(10000.00);
+            }
+
+            String RqUID = requestDTO.getRqUID();
+
+            ResponseDTO responseDTO = new ResponseDTO();
+
+            requestDTO.setRqUID(RqUID);
+            requestDTO.setClientId(clientId);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency("RUB");
+            responseDTO.setBalance("900");
+            responseDTO.setMaxLimit(maxLimit);
+
+            log.error("***** Запрос *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("***** Ответ *****" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+
+            long pacing = ThreadLocalRandom.current().nextLong(100, 500);
+            long end_time = System.currentTimeMillis();
+            if (end_time - start_time < pacing)
+                Thread.sleep(pacing - (end_time - start_time));
+
+            return responseDTO;
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 }
